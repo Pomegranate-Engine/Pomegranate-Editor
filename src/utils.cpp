1fee22c7df93fa32caf1cd84e6b1a72d0e20e26b
Large-Guy <r.lebgue@gmail.com>
2024-03-24 17:44:56 -0700

    Didn't work, trying again

--- src/utils.cpp
+++ src/utils.cpp
@@ -3,16 +3,15 @@ std::string scuffy_demangle(const char* name)
 {
 	std::string demangled_name = boost::core::demangle(name);
 
-	// Remove the class/struct/enum/union keyword at the beginning
-	size_t pos = demangled_name.find_first_of(' ');
-	if (pos != std::string::npos)
-		demangled_name = demangled_name.substr(pos + 1);
-
 	// Remove __cdecl(void) from the end
 	std::string cdecl_str = "__cdecl(void)";
-	pos = demangled_name.find(cdecl_str);
+	size_t pos = demangled_name.find(cdecl_str);
 	if (pos != std::string::npos)
 		demangled_name.erase(pos, cdecl_str.length());
+
+	// Remove the parentheses if they exist at the end
+	while (!demangled_name.empty() && demangled_name.back() == '(')
+		demangled_name.pop_back();
 
 	return demangled_name;
 }
