b3d9fc785845f3ca3001142aedc3517eb0cd0b41
Large-Guy <r.lebgue@gmail.com>
2024-04-07 20:32:23 -0700

    Removed test code

--- res/PlayerComponent.lua
+++ res/PlayerComponent.lua
@@ -1,15 +1,3 @@
-local g = Group.find("root")
-local e = Entity.new("Thing")
-
-local test_g = Group.new("test")
-test_g:add(e)
-
-local transform = e:add_component("class Pomegranate::Transform")
-g:add(test_g)
-print("Transform Component: ", transform.pos)
-
-transform.pos = Vec2.new(64, 64)
-
 return {
     name="PlayerComponent",
     velocity=Vec2.new(0,0),
